58f5e32c07fa06740c911153b7996e34a6d028b6
Arun Selva Kumar <b.arunselvakumar@gmail.com>
2019-07-09 11:57:07 +0530

    TSLint Configuration Merged

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Patch, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+    @Get()
+    getHello(): string {
+        return this.appService.getHello();
+    }
+
+    @Post()
+    saveHello(): string {
+        return 'Hello World';
+    }
+
+    @Patch()
+    updateHello(): string {
+        return 'New Hello World';
+    }
 }
