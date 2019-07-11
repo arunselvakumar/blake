b47199b7484ba8e6c791673a24ef8f6e48bf796d
prakash <prakash.selvaraj@outlook.com>
2019-07-12 00:04:41 +0530

    mistake resolved

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,11 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ServicesModule } from './services/services.module';
 import { SharedModule } from './shared/shared.module';
-import { AuthenticationModule } from './authentication/authentication.module';
-import { HttpclientModule } from './httpclient/httpclient.module';
+
 
 @Module({
     imports: [
         ServicesModule,
-        HttpclientModule,
-        AuthenticationModule,
         MongooseModule.forRoot('mongodb://arunselvakumar:abz123@ds052629.mlab.com:52629/blake'),
         SharedModule,
     ],
