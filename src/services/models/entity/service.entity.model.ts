4951e94843d2d06e1eef648b56975bd0c5113ae5
prakash <prakash.selvaraj@outlook.com>
2019-07-11 23:46:17 +0530

    Services started

--- src/services/models/entity/service.entity.model.ts
+++ src/services/models/entity/service.entity.model.ts
@@ -1,1 +1,33 @@
-export interface ServiceEntityModel {}
+import * as mongoose from 'mongoose';
+
+export const ServiceEntitySchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    phone: { type: String, required: true },
+    upTime: { type: Number },
+    servicableDistance: { type: Number },
+    isOffline: { type: Boolean },
+    location: {
+        type: {
+            type: String, // Don't do `{ location: { type: String } }`
+            enum: ['Point'], // 'location.type' must be 'Point'
+            required: true
+        },
+        coordinates: {
+            type: [Number],
+            required: true
+        },
+        required: true
+    }
+})
+
+export interface ServiceEntityModel extends mongoose.Document {
+    readonly name : string,
+    readonly phone: string,
+    readonly upTime: number,
+    readonly serviceableDistance: string,
+    readonly isOffline: boolean,
+    readonly location : {
+        type: {type: "Point"},
+        coordinates: [number]
+    }
+}
